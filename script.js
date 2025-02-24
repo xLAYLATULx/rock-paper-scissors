ee7325d6fe0e47b8bc844ce1b9c36c1105b5ecb8
Laylatul Firdaus <laylatul.firdaus@outlook.com>
2025-02-24 23:04:50 +0000

    Added UI to game

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 let humanScore = 0;
 let computerScore = 0;
 
+const rockbtn = document.createElement("button");
+const scissorbtn = document.createElement("button");
+const paperbtn = document.createElement("button");
+
+rockbtn.textContent = "Rock";
+scissorbtn.textContent = "Scissors";
+paperbtn.textContent = "Paper";
+
+document.body.appendChild(rockbtn);
+document.body.appendChild(scissorbtn);
+document.body.appendChild(paperbtn);
+
+rockbtn.addEventListener("click", function() {
+  playRound("rock", getComputerChoice());
+});
+
+scissorbtn.addEventListener("click", function() {
+  playRound("scissors", getComputerChoice());
+});
+
+paperbtn.addEventListener("click", function() {
+  playRound("paper", getComputerChoice());
+});
+
+const result = document.createElement("div");
+document.body.appendChild(result);
+
 function getComputerChoice() {
   let random = Math.random(0, 3);
   if (random < 0.33) {
@@ -15,32 +42,32 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  let choice = prompt("Enter your choice: ");
-  console.log(choice);
-  return choice;
-}
-
 function playRound(humanChoice, computerchoice) {
+  const computerAnswer = document.createElement("p");
+  computerAnswer.textContent = "Computer chose: " + computerchoice.charAt(0).toUpperCase() + computerchoice.slice(1) + ".";
+  result.appendChild(computerAnswer);
+  const points = document.createElement("p");
+  result.appendChild(points);
   humanChoice = humanChoice.toLowerCase();
   if (humanChoice === computerchoice) {
-    console.log("Draw! No points to anyone.");
+    points.textContent = "Draw! No points to anyone. Score: Human " + humanScore + " Computer " + computerScore;
   } else if ((humanChoice === "rock" && computerchoice === "scissors") || (humanChoice === "paper" && computerchoice === "rock") || (humanChoice === "scissors" && computerchoice === "paper")) {
     humanScore++;
-    console.log("You Win! One Point to Human. " + humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1) + " beats " + computerchoice.charAt(0).toUpperCase() + computerchoice.slice(1) + ".");
+    points.textContent = "One Point to Human! " + humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1) + " beats " + computerchoice.charAt(0).toUpperCase() + computerchoice.slice(1) + ". Score: Human " + humanScore + " Computer " + computerScore;
   } else {
     computerScore++;
-    console.log("You Lose! One Point to Computer. " + computerchoice.charAt(0).toUpperCase() + computerchoice.slice(1) + " beats " + humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1) + ".");
+    points.textContent = "One Point to Computer! " + computerchoice.charAt(0).toUpperCase() + computerchoice.slice(1) + " beats " + humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1) + ". Score: Human " + humanScore + " Computer " + computerScore;
+  }
+
+  if(humanScore === 5 || computerScore === 5){
+    rockbtn.disabled = true;
+    scissorbtn.disabled = true;
+    paperbtn.disabled = true;
+  }
+
+  if(humanScore === 5){
+    result.textContent = "Human wins!";
+  } else if(computerScore === 5){
+    result.textContent = "Computer wins!";
   }
 }
-
-function playGame() {
-  for(let i=0; i<5; i++){
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-  }
-  console.log("Your Score: " + humanScore + ". Computer Score: " + computerScore + ".");
-}
-
-playGame();
